f99d5ad08097970acef1e544b100da142ace1e2e
d.alves <d.alves@unlimint.com>
2025-10-28 14:10:06 -0300

    First commit

---
+++ src/test/java/br/com/kualit/devices/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kualit.devices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
